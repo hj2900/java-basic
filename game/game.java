0f616b8c3ec57f884d27922141232480a77e8fdb
hj2900 <hjypyp@gmail.com>
2024-05-21 16:21:18 +0900

    main

---
+++ game/game.java
@@ -0,0 +1,17 @@
+package game;
+
+public class game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new game_box();
+	}
+
+}
+
+class game_box{
+	
+	public game_box() {
+		
+	}
+}
--- game/hello.java
+++ game/hello.java
@@ -4,6 +4,9 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		sb.append("유재석");
+		sb.append("강호동");
 		System.out.println("hello world");
 	}
 
